7e17e26b1946c5a73077f3e54fb926d1551ad9ac
satyam <kh.satyam09@gmail.com>
2019-02-19 16:34:42 +0530

    updated app

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -4,59 +4,29 @@ import java.util.Scanner;
  * Hello world!
  *
  */
-public class App 
-{
+public class App
+  {
     public static void main(String args[])
      {
-       float a, b, res;
-       char choice, ch;
-       Scanner scan = new Scanner(System.in);
-	      
-      	do
-         {
-           System.out.print("1. Addition\n");
-           System.out.print("2. Subtraction\n");
-           System.out.print("3. Multiplication\n");
-           System.out.print("4. Division\n");
-           System.out.print("5. Exit\n\n");
-           System.out.print("Enter Your Choice : ");
-           choice = scan.next().charAt(0);
-           switch(choice)
-            {
-              case '1' : System.out.print("Enter Two Number : ");
-                         a = scan.nextFloat();
-                         b = scan.nextFloat();
-                         res = a + b;
-                         System.out.print("Result = " + res);
-	                 break;															                       
-	      case '2' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a - b;
-	                 System.out.print("Result = " + res);
-	                 break;
-	 
-	      case '3' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a * b;
-	                 System.out.print("Result = " + res);
-	                 break;
-	 
-	      case '4' : System.out.print("Enter Two Number : ");
-	                 a = scan.nextFloat();
-	                 b = scan.nextFloat();
-	                 res = a / b;
-	                 System.out.print("Result = " + res);
-	                 break;
-	 
-	      case '5' : System.exit(0);
-	                 break;
-	
-	      default : System.out.print("Wrong Choice!!!");
-	                break;
-	   }
-																	            System.out.print("\n---------------------------------------\n");
-	}while(choice != 5);       
+        String s2 = args[1];
+        char ch = s2.charAt(0);
+        int i,j,total = 0;
+        i = Integer.parseInt(args[0]);
+        j = Integer.parseInt(args[2]);
+        
+        
+        switch(ch)
+        {
+        case '+' :
+            total = i + j;
+            break;
+        case '*' :
+             total = i *j;
+             System.out.println("Multiply");
+             break;
+                // other symbols to be included later...
+        }
+        
+        System.out.println(args[0] + ch + args[2] + " " + total);     
     }
 }
